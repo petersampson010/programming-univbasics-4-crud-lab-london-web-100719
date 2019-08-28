41976829f3b0c6f880f3de2f7bd8349bac1c8d5d
Peter Sampson <petersampson010@gmail.com>
2019-08-28 13:05:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,12 +14,12 @@ def add_element_to_start_of_array(array, element)
   array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array, element)
-  array.pop(element)
+def remove_element_from_end_of_array(array)
+  array.pop()
 end
 
-def remove_element_from_start_of_array(array, element)
-  array.shift(element)
+def remove_element_from_start_of_array(array)
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
